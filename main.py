7d9954c37d4b2d9b5d1641f325811f4207070714
Ian Whitesel <iwhitese@kent.edu>
2022-01-16 12:50:00 -0500

    Quick Sort implemented. Label added.

--- main.py
+++ main.py
@@ -53,28 +53,30 @@ class VisualElement(Frame):
         logging.debug("Interpolation: " + str(self.posX) + ", " + str(self.posY))
         self.canvas.coords(self.myShape, self.posX, self.posY, w, h)
         if abs(self.posX - self.targetX) < 0.05 and abs(self.posY - self.targetY) < 0.05:
-            self.animate()
             return True
         return False
 
-    def animate(self):
+    def changeColor(self, argColorStr):
         # change an object's fill color
-        self.canvas.itemconfig(self.myShape, fill="green")
+        self.canvas.itemconfig(self.myShape, fill=argColorStr)
+
+    def swap(self, visualToSwap, argRoot):
+        # swap the graphical representations
+        tempX = visualToSwap.targetX
+        tempY = visualToSwap.targetY
+        visualToSwap.setMoveTarget(self.targetX, self.targetY)
+        self.setMoveTarget(tempX, tempY)
+        # swap the values
+        while not visualToSwap.move() and not self.move():
+            argRoot.update()
 
 
 def scrambleElements(arr, argRoot):
-    min_idx = 0
+    idx = 0
     for i in range(len(arr)):
-        min_idx = random.randint(1, len(arr) - 1)
-        # swap the graphical representations
-        tempX = arr[i][2].targetX
-        tempY = arr[i][2].targetY
-        arr[i][2].setMoveTarget(arr[min_idx][2].targetX, arr[min_idx][2].targetY)
-        arr[min_idx][2].setMoveTarget(tempX, tempY)
-        # swap the smallest value to the beginning of the array, and put the bigger value where the smaller was at
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        while not arr[i][2].move() and not arr[min_idx][2].move():
-            argRoot.update()
+        idx = random.randint(1, len(arr) - 1)
+        arr[i][2].swap(arr[idx][2], argRoot)
+        arr[i], arr[idx] = arr[idx], arr[i]
 
 
 def selectionSort(arr, argRoot):
@@ -83,15 +85,9 @@ def selectionSort(arr, argRoot):
         for j in range(i + 1, len(arr)):
             if arr[min_idx][1] > arr[j][1]:
                 min_idx = j
-        # swap the graphical representations
-        tempX = arr[i][2].targetX
-        tempY = arr[i][2].targetY
-        arr[i][2].setMoveTarget(arr[min_idx][2].targetX, arr[min_idx][2].targetY)
-        arr[min_idx][2].setMoveTarget(tempX, tempY)
+        arr[i][2].swap(arr[min_idx][2], argRoot)
         # swap the smallest value to the beginning of the array, and put the bigger value where the smaller was at
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        while not arr[i][2].move() and not arr[min_idx][2].move():
-            argRoot.update()
     return True
 
 
@@ -102,36 +98,51 @@ def bubbleSort(arr, argRoot):
         for i in range(len(arr) - 1):
             if arr[i][1] > arr[i + 1][1]:
                 sorted = False
-                # swap the graphical representations
-                tempX = arr[i][2].targetX
-                tempY = arr[i][2].targetY
-                arr[i][2].setMoveTarget(arr[i + 1][2].targetX, arr[i + 1][2].targetY)
-                arr[i + 1][2].setMoveTarget(tempX, tempY)
                 # swap the values
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                while not arr[i][2].move() and not arr[i + 1][2].move():
-                    argRoot.update()
+                arr[i][2].swap(arr[i + 1][2], argRoot)
     return sorted
 
 
 def recursiveInsertionSort(arr, n, argRoot):
     if n <= 1:
         return
-    recursiveInsertionSort(arr, n-1, argRoot)
+    recursiveInsertionSort(arr, n - 1, argRoot)
     j = n - 2
     last = arr[n - 1]
     while j >= 0 and arr[j][1] > last[1]:
-        # swap the graphical representations
-        tempX = arr[j][2].targetX
-        tempY = arr[j][2].targetY
-        arr[j][2].setMoveTarget(last[2].targetX, last[2].targetY)
-        last[2].setMoveTarget(tempX, tempY)
-        # swap the values
-        while not arr[j][2].move() and not last[2].move():
-            argRoot.update()
+        arr[j][2].swap(last[2], argRoot)
         arr[j + 1] = arr[j]
         j -= 1
     arr[j + 1] = last
+
+
+# low is the first index
+# high is the last index
+def quickSort(arr, low, high, argRoot):
+    if low < high:
+        pivot = partition(arr, low, high, argRoot)
+        # sort to the left of the pivot
+        quickSort(arr, low, pivot - 1, argRoot)
+        # sort to the right of the pivot
+        quickSort(arr, pivot + 1, high, argRoot)
+
+
+def partition(arr, low, high, argRoot):
+    # pivot is the element to the furthest right
+    pivot = arr[high]
+    pivot[2].changeColor("green")
+    # i is the index of the smaller element and indicates the right position of pivot found so far
+    i = (low - 1)
+    for j in range(low, high):
+        # if the current element is smaller than the pivot
+        if arr[j][1] < pivot[1]:
+            i += 1
+            arr[j][2].swap(arr[i][2], argRoot)
+            arr[j], arr[i] = arr[i], arr[j]
+    arr[i + 1][2].swap(arr[high][2], argRoot)
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 
 
 def main():
@@ -144,12 +155,18 @@ def main():
     root.geometry("1040x720+400+100")
     # root.attributes("-fullscreen", True)
     logging.info("Adding widgets")
-    canvas = Canvas(bg="Cyan")
+    # pack label
+    labelStr = tkinter.StringVar()
+    label = tkinter.Label(root, textvariable=labelStr)
+    label.pack()
+    # pack canvas
+    canvas = Canvas(root, bg="Cyan")
     canvas.pack(fill=BOTH, expand=True)
-    # Debug array of elements
     elements = []
+    # Debug array of elements
+    # values = [10, 70, 30, 50, 90, 20, 80, 40, 60,  100]
     values = []
-    for i in range(0, 89):
+    for i in range(0, 80):
         values.append(random.randint(1, 700))
     xPos = 1
     yPos = 720
@@ -166,11 +183,20 @@ def main():
         if keyboard.is_pressed("esc"):
             exitApplication = True
         if keyboard.is_pressed("e"):
+            labelStr.set("Recursive Insertion Sort")
             recursiveInsertionSort(elements, len(elements), root)
+            labelStr.set("Scrambling Array...")
             scrambleElements(elements, root)
+            labelStr.set("Bubble Sort")
             bubbleSort(elements, root)
+            labelStr.set("Scrambling Array...")
             scrambleElements(elements, root)
+            labelStr.set("Selection Sort")
             selectionSort(elements, root)
+            labelStr.set("Scrambling Array...")
+            scrambleElements(elements, root)
+            labelStr.set("Quick Sort")
+            quickSort(elements, 0, len(elements) - 1, root)
         # process movement
         # for obj in animatedObjects:
         # obj.move()
